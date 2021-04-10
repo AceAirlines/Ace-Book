399ed21222981dd6d19dbd90e8e9d2c563387562
Drew Crocker <drewcrocker23@gmail.com>
2021-04-10 14:38:13 -0500

    Version 1.5.1 -  Auth and Book

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const Security = require(__dirname, '/lib/security/security.js')
+const path = require('path')
+const Security = require(path.join(__dirname, '/lib/security/security.js'))
 const session = require('express-session')
 const cookieParser = require('cookie-parser')
 const axios = require('axios')
+const { RSA_NO_PADDING } = require('constants')
 const app = express()
 
 app.use(session({
@@ -14,88 +16,85 @@ app.use(session({
 }))
 
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(__dirname, "/public/static"))
+app.use(express.static(path.join(__dirname, "/public/static")))
 app.set('view engine', 'ejs')
 app.set('views', 'public/views')
 
 /* Render static routing */
 app.get('/', (req, res) => { res.render('login') })
-app.get('/home', async (req, res) => { new Security({ auth: req.session.authtoken, redirect: 'book', options: {} }).authenticateTokenAccess(req, res) })
-app.get('/checkin', async (req, res) => { new Security({ auth: req.session.authtoken, redirect: 'checkin', options: {} }).authenticateTokenAccess(req, res) })
-app.get('/myflights', async (req, res) => { new Security({ auth: req.session.authtoken, redirect: 'myflights', options: {} }).authenticateTokenAccess(req, res) })
-
-/* Render flight template */
-/*app.get('/flightstatus', async (req, res) => { 
-	new Security({ 
-		auth: req.session.authtoken, 
-	}).authenticateTokenAccessAsync(req.url).then((response) => {
-		axios.post('https://ace-app-version-2.aceairlines.repl.co/returnFlights', {
-			username: req.session.userconfiguration
-		})
-			.then(({ data }) => {
-				console.log(data)
-			})
-	}).catch(({ err }) => {
-		res.render('404', { errorType: err })
-	})
-})*/
+app.get('/home', async (req, res) => { 
+	new Security().verifyUserTokenAsync(req.sessionID).then(() => {
+		res.render('book')
+	}).catch((e) => { console.log(e); res.render('404', { errorType: 'internal' }) })
+})
+app.get('/checkin', async (req, res) => { 
+	new Security().verifyUserTokenAsync(req.sessionID).then(() => {
+		res.render('checkin')
+	}).catch((e) => { console.log(e); res.render('404', { errorType: 'internal' }) })
+})
+app.get('/myflights', async (req, res) => {
+	new Security().verifyUserTokenAsync(req.sessionID).then(() => {
+		res.render('myflights')
+	}).catch((e) => { console.log(e); res.render('404', { errorType: 'internal' }) })
+})
 
 /* Authenticate with auth0 through app request */
-app.post('/auth0', (req, res) => { new Security(req.body).authenticateFirstTimeLogin(req, res) })
+app.post('/auth0', (req, res) => { 
+	new Security(req.body).authenticateFirstTimeLogin(req.sessionID).then(() => {
+		res.redirect('/home')
+	}).catch((e) => {
+		res.render('404', { errorType: 'internal' })
+	})
+})
 
 /* Configure book from GET /book route */
 app.post('/book', (req, res) => {
-	var tripType = req.body['trip-type'] === 'oneway' ? 'ONE WAY' : 'ROUND TRIP'
-	axios.post('https://ace-app-version-2.aceairlines.repl.co/destinations')
-		.then(({ data }) => {
-			/* Search to create session packet */
-			var filteredDepartures = []
-			data.forEach((departure) => {
-				if (req.body.depart.toUpperCase() === departure.icao.toUpperCase() || req.body.depart.toUpperCase() === departure.state.toUpperCase() || req.body.depart.toUpperCase() === departure.city.toUpperCase()) {
-					filteredDepartures.push(departure)
-				} else { }
-			})
-
-			/* Searching arrival input */
-			var filteredArrivals = []
-			var depatureInstance = 0
-			if (filteredDepartures.length != 0) {
-				filteredDepartures[0].departures.forEach((arrival) => {
-					if (req.body.arrive.toUpperCase() === arrival.arrival.icao.toUpperCase() || req.body.arrive.toUpperCase() === arrival.arrival.state.toUpperCase() || req.body.arrive.toUpperCase() === arrival.arrival.city.toUpperCase()) {
-						filteredArrivals.push({
-							departure: filteredDepartures[0],
-							arrivalIndex: depatureInstance
-						})
+	new Security().verifyUserTokenAsync(req.sessionID).then((sessionComponent) => {
+		var tripType = req.body['trip-type'] === 'oneway' ? 'ONE WAY' : 'ROUND TRIP'
+		axios.post('https://ace-app-version-2.aceairlines.repl.co/destinations')
+			.then(({ data }) => {
+				/* Search to create session packet */
+				var filteredDepartures = []
+				data.forEach((departure) => {
+					if (req.body.depart.toUpperCase() === departure.icao.toUpperCase() || req.body.depart.toUpperCase() === departure.state.toUpperCase() || req.body.depart.toUpperCase() === departure.city.toUpperCase()) {
+						filteredDepartures.push(departure)
 					} else { }
-					depatureInstance++
 				})
-			} else { }
-
-			req.body['trip-type'] = tripType
-			req.session.routetemp = { suggested: filteredArrivals, inputted: req.body }
-			res.redirect('/compare')
-
-		})
+				/* Searching arrival input */
+				var filteredArrivals = []
+				var depatureInstance = 0
+				if (filteredDepartures.length != 0) {
+					filteredDepartures[0].departures.forEach((arrival) => {
+						if (req.body.arrive.toUpperCase() === arrival.arrival.icao.toUpperCase() || req.body.arrive.toUpperCase() === arrival.arrival.state.toUpperCase() || req.body.arrive.toUpperCase() === arrival.arrival.city.toUpperCase()) {
+							filteredArrivals.push({
+								departure: filteredDepartures[0],
+								arrivalIndex: depatureInstance
+							})
+						} else { }
+						depatureInstance++
+					})
+				} else { }
+				req.body['trip-type'] = tripType
+				sessionComponent.set({ routetemp: { suggested: filteredArrivals, inputted: req.body }})
+				res.redirect('/compare')
+			})
+	}).catch((e) => { console.log(e); res.render('404', { errorType: 'internal' }) })
 })
 
 /* Compare your flight options */
 app.get('/compare', (req, res) => { 
-	new Security({ 
-		auth: req.session.authtoken, 
-		redirect: 'compareFlight', 
-		options: req.session.routetemp 
-	}).authenticateTokenAccess(req, res) 
+	new Security().verifyUserTokenAsync(req.sessionID).then((sessionComponent) => {
+		res.render('compareFlight', sessionComponent.get('routetemp'))
+	}).catch((e) => { console.log(e); res.render('404', { errorType: 'internal' }) })
 })
 
 /* Configure final POST route to push destination */
 app.post('/request', (req, res) => {
-	new Security({ 
-		auth: req.session.authtoken
-	}).authenticateTokenAccessAsync(req.url).then((response) => {
+	new Security().verifyUserTokenAsync(req.sessionID).then((sessionComponent) => {
 		axios.post('https://ace-app-version-2.aceairlines.repl.co/savebook', { 
 			data: {
 				digest: JSON.parse(Object.keys(req.body)[0]),
-				username: req.session.userconfiguration
+				username: sessionComponent.get('userconfiguration')
 			}
 		})
 			.then(({ data }) => {
@@ -107,13 +106,10 @@ app.post('/request', (req, res) => {
 			}).catch(() => {
 				res.render('404', { errorType: 'internal'})
 			})
-	}).catch((err) => {
-		console.log(err)
-		res.render('404', { errorType: err.err})
-	})
+	}).catch((e) => { console.log(e); res.render('404', { errorType: 'internal' }) })
 })
 
 /* Basic port listener */
 app.listen(3000, () => {
-	console.log('Listening.\n\nSERVER REQUESTS on 127.0.0.1::')
+	console.log('Listening::3000')
 })
